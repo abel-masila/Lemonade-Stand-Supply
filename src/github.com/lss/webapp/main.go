be198fe5ba01f4533179a69a34ea7ae6f3281832
abel boniface masila <abelmasila@gmail.com>
2018-01-12 19:16:23 -0800

    Added Template support

--- src/github.com/lss/webapp/main.go
+++ src/github.com/lss/webapp/main.go
@@ -1,9 +1,33 @@
 package main
 
 import (
+	"html/template"
+	"log"
 	"net/http"
 )
 
 func main() {
-	http.ListenAndServe(":8000", http.FileServer(http.Dir("public")))
+	templates := populateTemplates()
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		requestedFile := r.URL.Path[1:]
+		t := templates.Lookup(requestedFile + ".html")
+		if t != nil {
+			err := t.Execute(w, nil)
+			if err != nil {
+				log.Fatal(err)
+			}
+		} else {
+			w.WriteHeader(http.StatusNotFound)
+		}
+	})
+	http.Handle("/img/", http.FileServer(http.Dir("public")))
+	http.Handle("/css/", http.FileServer(http.Dir("public")))
+
+	http.ListenAndServe(":8000", nil)
+}
+func populateTemplates() *template.Template {
+	result := template.New("templates")
+	const basePath = "templates"
+	template.Must(result.ParseGlob(basePath + "/*.html"))
+	return result
 }
